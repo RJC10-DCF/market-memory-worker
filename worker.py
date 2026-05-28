8882dc901371fd2503990e7439c20b0a721bbaec
RJC10-DCF <riccardocolombo@yahoo.it>
2026-05-28 18:22:17 +0200

    Update worker.py

--- worker.py
+++ worker.py
@@ -8,7 +8,7 @@ print("Market Memory worker started...")
 while True:
     try:
         print("Checking pending jobs...")
-        subprocess.run(["py", "process_jobs.py"], check=False)
+        subprocess.run(["python", "process_jobs.py"], check=False)
     except Exception as e:
         print("Worker error:", e)
 
